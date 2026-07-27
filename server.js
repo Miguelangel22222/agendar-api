5c27bc009def4b7a337aa8c61c647e6c2b9d1009
Miguelangel22222 <miguelangel22222@gmail.com>
2026-07-26 23:20:36 -0300

    fix: PORT no estaba definido

--- server.js
+++ server.js
@@ -39,4 +39,5 @@ app.post(['/agendar', '/api/agendar'], async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
